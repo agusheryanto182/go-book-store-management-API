5ff8fa7c30fd90fba6620339d5bf3ce42fc63195
Agus Heryanto <agusheryantodev@gmail.com>
2023-09-06 09:09:31 +0700

    update documentation

--- package/utils/utils.go
+++ package/utils/utils.go
@@ -7,8 +7,14 @@ import (
 )
 
 func ParseBody(r *http.Request, x interface{}) {
-	if body, err := ioutil.ReadAll(r.Body); err == nil {
+	// Membaca seluruh isi body dari permintaan HTTP
+	body, err := ioutil.ReadAll(r.Body)
+
+	// Memeriksa apakah pembacaan body berhasil
+	if err == nil {
+		// Menguraikan isi body yang berisi JSON ke dalam variabel x yang ditentukan oleh pengguna
 		if err := json.Unmarshal([]byte(body), x); err != nil {
+			// Jika terjadi kesalahan dalam penguraian JSON, fungsi ini mengembalikan nilai
 			return
 		}
 	}
